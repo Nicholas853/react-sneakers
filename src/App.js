e61804b1ed7cfac0c755c20365778a815a0df7c5
Nick <nikolaj.sprinc@gmail.com>
2023-09-21 17:46:14 +0300

    Completed header

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
+/* eslint-disable jsx-a11y/alt-text */
 function App() {
   return (
-    <div className="wrapper">
-      <header className="d-flex">
-        <img width={40} height={40} src="/img/logo.png"/>
-        <div className="headerLeft">
-          <svg />
-          <div className="headerInfo">
-            <h3>React Sneakers</h3>
+    <div className="wrapper clear">
+      <header className="d-flex justify-between align-center p-40">
+        <div className="d-flex align-center">
+          <img width={40} height={40} src="/img/logo.png"/>
+          <div>
+            <h3 className="text-uppercase">React Sneakers</h3>
             <p>Магазин найкращих кросівок</p>
           </div>
         </div>
-        <ul >
-          <li>
-          <img width={18} height={18} src="/img/cart.svg"/>           
-           <span>1205 грн.</span>
+        <ul className="d-flex">
+          <li className="mr-30">
+            <img className="mr-10" width={18} height={18} src="/img/cart.svg"/>           
+            <span>1205 грн.</span>
           </li>
-          <li>
-          <img width={18} height={18} src="/img/user.svg"/>
+          <li className="mr-10">
+            <img width={18} height={18} src="/img/user.svg"/>
           </li>
         </ul>
       </header>
-      <div className="content">
+      <div className="content p-40">
         <h1>
           Всі кросівки
         </h1>
