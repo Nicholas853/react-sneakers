209f41ae28b02de1a0420d477b3affdc11ca39a3
Nick <nikolaj.sprinc@gmail.com>
2023-09-21 17:22:03 +0300

    Prepared to work
    Install scss;
    Install macro-css library;
    Delete all useless things.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,30 @@
 function App() {
   return (
-    <div className="App">
-      123
+    <div className="wrapper">
+      <header className="d-flex">
+        <img width={40} height={40} src="/img/logo.png"/>
+        <div className="headerLeft">
+          <svg />
+          <div className="headerInfo">
+            <h3>React Sneakers</h3>
+            <p>Магазин найкращих кросівок</p>
+          </div>
+        </div>
+        <ul >
+          <li>
+          <img width={18} height={18} src="/img/cart.svg"/>           
+           <span>1205 грн.</span>
+          </li>
+          <li>
+          <img width={18} height={18} src="/img/user.svg"/>
+          </li>
+        </ul>
+      </header>
+      <div className="content">
+        <h1>
+          Всі кросівки
+        </h1>
+      </div>
     </div>
   );
 }
