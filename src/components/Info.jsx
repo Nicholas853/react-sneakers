6b7d572514954fe40ef728cba056401441a711b9
Nick <nikolaj.sprinc@gmail.com>
2023-11-27 13:18:16 +0200

    fix

--- src/components/Info.jsx
+++ src/components/Info.jsx
@@ -10,7 +10,7 @@ const Info = ({title, image, description, }) => {
             <h2>{title}</h2>
             <p className="opacity-6">{description}</p>
             <button onClick={() => setCartOpened(false)} className="greenButton">
-                <img width={16} height={14} src="/img/arrow.svg" alt="Arrow"/>Повернутись назад
+                <img width={14} height={12} src="/img/arrow2.png" alt="Arrow"/>Повернутись назад
             </button>
         </div>
     )
